ff240187b28fca8f1479a0c361b0e14f13cf0ed0
elekit <kit93@hotmail.it>
2018-04-21 17:59:03 +0200

    public bool in the menu to skip tuning session

--- assets/Scripts/Menu/TuningController.cs
+++ assets/Scripts/Menu/TuningController.cs
@@ -20,202 +20,234 @@ public class TuningController : MonoBehaviour
 	public GameObject tuningInfos, hands, acceptText, redoText, handController, background, reText, leText, rfText, lfText, rudText, rrdText, ludText, lrdText;
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		acceptText.SetActive (false);
-		redoText.SetActive (false);
+		acceptText.SetActive(false);
+		redoText.SetActive(false);
 		time = maxTime;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 //		if(Input.GetKeyDown(KeyCode.Space))
 //			Time.timeScale = 1f;
 
 
-		if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-			distance = Mathf.Abs (handController.GetComponent<MyHandController>().hands [0].PalmPosition.x - handController.GetComponent<MyHandController>().hands [1].PalmPosition.x);
+		if (handController.GetComponent<HandController>().visibleHands == 2)
+		{
+			distance = Mathf.Abs(handController.GetComponent<HandController>().hands[0].PalmPosition.x - handController.GetComponent<HandController>().hands[1].PalmPosition.x);
 		}
 
-		if (background.GetComponent<Renderer> ().isVisible && !startTuning) {
-			tuningInfos.GetComponent<TuningInfos> ().SetFirstTimeText ();
+		if (PlayerSaveData.playerData.GetFirstTimePlaying() && background.GetComponent<Renderer>().isVisible && !isTuning)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetFirstTimeText();
+			isTuning = true;
 			startTuning = true;
 		}
 
-		if (startTuning) {
-			if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-				if (distance >= minDistance && distance <= maxDistance) {
+		if (PlayerSaveData.playerData.GetTuningTooOld() && background.GetComponent<Renderer>().isVisible && !isTuning)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetFirstTimeText();
+			isTuning = true;
+			startTuning = true;
+		}
+
+
+		if (startTuning)
+		{
+			if (handController.GetComponent<HandController>().visibleHands == 2)
+			{
+				if (distance >= minDistance && distance <= maxDistance)
+				{
 					startMaxVertical = true;
-					hands.GetComponent<AnimatedTextureExtendedUV> ().rowNumber = 1;
+					hands.GetComponent<AnimatedTextureExtendedUV>().rowNumber = 1;
 					startTuning = false;
 				}
 			}
 		}
 
-		if (startMaxVertical) {
-			tuningInfos.GetComponent<TuningInfos> ().SetHandsUpText ();
-			if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-				if (!timer) {
-					GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = time.ToString ();
-					InvokeRepeating ("Countdown", 1f, 1f);
+		if (startMaxVertical)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetHandsUpText();
+			if (handController.GetComponent<HandController>().visibleHands == 2)
+			{
+				if (!timer)
+				{
+					GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = time.ToString();
+					InvokeRepeating("Countdown", 1f, 1f);
 					timer = true;
 				}
 			}
 		}
 
-		if (startMinVertical) {
-			tuningInfos.GetComponent<TuningInfos> ().SetHandsDownText ();
-			if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-				if (!timer) {
-					GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = time.ToString ();
-					InvokeRepeating ("Countdown", 1f, 1f);
+		if (startMinVertical)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetHandsDownText();
+			if (handController.GetComponent<HandController>().visibleHands == 2)
+			{
+				if (!timer)
+				{
+					GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = time.ToString();
+					InvokeRepeating("Countdown", 1f, 1f);
 					timer = true;
 				}
 			}
 		}
 
-		if (startMaxHorizontal) {
-			tuningInfos.GetComponent<TuningInfos> ().SetHandsRightText ();
-			if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-				if (!timer) {
-					GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = time.ToString ();
-					InvokeRepeating ("Countdown", 1f, 1f);
+		if (startMaxHorizontal)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetHandsRightText();
+			if (handController.GetComponent<HandController>().visibleHands == 2)
+			{
+				if (!timer)
+				{
+					GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = time.ToString();
+					InvokeRepeating("Countdown", 1f, 1f);
 					timer = true;
 				}
 			}
 		}
 
-		if (startMinHorizontal) {
-			tuningInfos.GetComponent<TuningInfos> ().SetHandsLeftText ();
-			if (handController.GetComponent<MyHandController>().visibleHands == 2) {
-				if (!timer) {
-					GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = time.ToString ();
-					InvokeRepeating ("Countdown", 1f, 1f);
+		if (startMinHorizontal)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetHandsLeftText();
+			if (handController.GetComponent<HandController>().visibleHands == 2)
+			{
+				if (!timer)
+				{
+					GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = time.ToString();
+					InvokeRepeating("Countdown", 1f, 1f);
 					timer = true;
 				}
 			}
 		}
 
 
-		if (time == 0 && startMaxVertical) {
-			CancelInvoke ();
-			GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = "";
+		if (time == 0 && startMaxVertical)
+		{
+			CancelInvoke();
+			GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = "";
 			time = maxTime;
-			rightMaxVertical = GameObject.Find("MyHandController").GetComponent<RightXRotationStatsScript> ().GetXExtension ();
-			leftMaxVertical = GameObject.Find("MyHandController").GetComponent<LeftXRotationStatsScript> ().GetXExtension ();
-			hands.GetComponent<AnimatedTextureExtendedUV> ().rowNumber = 2;
+			rightMaxVertical = GameObject.Find("HandController").GetComponent<RightXRotationStatsScript>().GetXExtension();
+			leftMaxVertical = GameObject.Find("HandController").GetComponent<LeftXRotationStatsScript>().GetXExtension();
+			hands.GetComponent<AnimatedTextureExtendedUV>().rowNumber = 2;
 			startMaxVertical = false;
 			startMinVertical = true;
 			timer = false;
 
 		}
 
-		if (time == 0 && startMinVertical) {
-			CancelInvoke ();
-			GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = "";
+		if (time == 0 && startMinVertical)
+		{
+			CancelInvoke();
+			GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = "";
 			time = maxTime;
-			rightMinVertical = GameObject.Find("MyHandController").GetComponent<RightXRotationStatsScript> ().GetXExtension ();
-			leftMinVertical = GameObject.Find("MyHandController").GetComponent<LeftXRotationStatsScript> ().GetXExtension ();
-			hands.GetComponent<AnimatedTextureExtendedUV> ().rowNumber = 3;
+			rightMinVertical = GameObject.Find("HandController").GetComponent<RightXRotationStatsScript>().GetXExtension();
+			leftMinVertical = GameObject.Find("HandController").GetComponent<LeftXRotationStatsScript>().GetXExtension();
+			hands.GetComponent<AnimatedTextureExtendedUV>().rowNumber = 3;
 			startMinVertical = false;
 			startMaxHorizontal = true;
 			timer = false;
 		}
 
-		if (time == 0 && startMaxHorizontal) {
-			CancelInvoke ();
-			GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = "";
+		if (time == 0 && startMaxHorizontal)
+		{
+			CancelInvoke();
+			GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = "";
 			time = maxTime;
-			rightMaxHorizontal = GameObject.Find("MyHandController").GetComponent<RightYRotationStatsScript> ().GetYExtension ();
-			leftMaxHorizontal = GameObject.Find("MyHandController").GetComponent<LeftYRotationStatsScript> ().GetYExtension ();
-			hands.GetComponent<AnimatedTextureExtendedUV> ().rowNumber = 4;
+			rightMaxHorizontal = GameObject.Find("HandController").GetComponent<RightYRotationStatsScript>().GetYExtension();
+			leftMaxHorizontal = GameObject.Find("HandController").GetComponent<LeftYRotationStatsScript>().GetYExtension();
+			hands.GetComponent<AnimatedTextureExtendedUV>().rowNumber = 4;
 			startMaxHorizontal = false;
 			startMinHorizontal = true;
 			timer = false;
 		}
 
-		if (time == 0 && startMinHorizontal) {
-			CancelInvoke ();
-			GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = "";
+		if (time == 0 && startMinHorizontal)
+		{
+			CancelInvoke();
+			GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = "";
 			time = maxTime;
-			rightMinHorizontal = GameObject.Find("MyHandController").GetComponent<RightYRotationStatsScript> ().GetYExtension ();
-			leftMinHorizontal = GameObject.Find("MyHandController").GetComponent<LeftYRotationStatsScript> ().GetYExtension ();
+			rightMinHorizontal = GameObject.Find("HandController").GetComponent<RightYRotationStatsScript>().GetYExtension();
+			leftMinHorizontal = GameObject.Find("HandController").GetComponent<LeftYRotationStatsScript>().GetYExtension();
 			startMinHorizontal = false;
 			timer = false;
-			hands.GetComponent<AnimatedTextureExtendedUV> ().rowNumber = 0;
+			hands.GetComponent<AnimatedTextureExtendedUV>().rowNumber = 0;
 
-			Debug.Log ("Destra - MaxVerticale: " + rightMaxVertical + " MinVerticale: " + rightMinVertical + " MaxOrizzontale: " + rightMaxHorizontal + " MinOrizzontale: " + rightMinHorizontal);
-			Debug.Log ("Sinistra - MaxVerticale: " + leftMaxVertical + " MinVerticale: " + leftMinVertical + " MaxOrizzontale: " + leftMaxHorizontal + " MinOrizzontale: " + leftMinHorizontal);
-			if (CheckCoherence ())
+			Debug.Log("Destra - MaxVerticale: " + rightMaxVertical + " MinVerticale: " + rightMinVertical + " MaxOrizzontale: " + rightMaxHorizontal + " MinOrizzontale: " + rightMinHorizontal);
+			Debug.Log("Sinistra - MaxVerticale: " + leftMaxVertical + " MinVerticale: " + leftMinVertical + " MaxOrizzontale: " + leftMaxHorizontal + " MinOrizzontale: " + leftMinHorizontal);
+			if (CheckCoherence())
 				endTuning = true;
-			else {
-				hands.SetActive (false);
-				tuningInfos.GetComponent<TuningInfos> ().SetIncoherentText ();
-				redoText.SetActive (true);
+			else
+			{
+				hands.SetActive(false);
+				tuningInfos.GetComponent<TuningInfos>().SetIncoherentText();
+				redoText.SetActive(true);
 			}
 
 		}
 
-		if (endTuning) {
-			tuningInfos.GetComponent<TuningInfos> ().SetEndText ();
-			hands.SetActive (false);
-			StampStats ();
-			acceptText.SetActive (true);
-			redoText.SetActive (true);
+		if (endTuning)
+		{
+			tuningInfos.GetComponent<TuningInfos>().SetEndText();
+			hands.SetActive(false);
+			StampStats();
+			acceptText.SetActive(true);
+			redoText.SetActive(true);
 		}
 	}
 
 
-	public void SaveStats ()
+	public void SaveStats()
 	{
-		GeneralSaveData.generalData.UpdatePlayerTunings (PlayerSaveData.playerData.GetUserName (), leftMinHorizontal, leftMaxHorizontal,
+		GeneralSaveData.generalData.UpdatePlayerTunings(PlayerSaveData.playerData.GetUserName(), leftMinHorizontal, leftMaxHorizontal,
 			leftMinVertical, leftMaxVertical, rightMinHorizontal, rightMaxHorizontal, rightMinVertical, rightMaxVertical);
-		PlayerSaveData.playerData.SetPlayer (GeneralSaveData.generalData.GetPlayer (PlayerSaveData.playerData.GetUserName ()));
-		SceneManager.LoadSceneAsync ("Main_Menu");
+		PlayerSaveData.playerData.SetPlayer(GeneralSaveData.generalData.GetPlayer(PlayerSaveData.playerData.GetUserName()));
+		SceneManager.LoadSceneAsync("Main_Menu");
 	}
 
-	void Countdown ()
+	void Countdown()
 	{
 		time--;
-		GameObject.Find ("Countdown Text").GetComponent<TextMesh> ().text = time.ToString ();
+		GameObject.Find("Countdown Text").GetComponent<TextMesh>().text = time.ToString();
 	}
 
-	public void Redo ()
+	public void Redo()
 	{
 		endTuning = false;
-		HideStats ();
-		redoText.SetActive (false);
-		tuningInfos.GetComponent<TuningInfos> ().SetRedoText ();
-		hands.SetActive (true);
+		HideStats();
+		redoText.SetActive(false);
+		tuningInfos.GetComponent<TuningInfos>().SetRedoText();
+		hands.SetActive(true);
 		startTuning = true;
 	}
 
-	void StampStats ()
+	void StampStats()
 	{
-		reText.GetComponent<TextMesh> ().text = "Estensione destra: " + Mathf.Abs (rightMaxVertical).ToString ("n2") + "°";
-		rfText.GetComponent<TextMesh> ().text = "Flessione destra: " + Mathf.Abs (rightMinVertical).ToString ("n2") + "°";
-		rudText.GetComponent<TextMesh> ().text = "Dev ulnare destra: " + Mathf.Abs (rightMaxHorizontal).ToString ("n2") + "°";
-		rrdText.GetComponent<TextMesh> ().text = "Dev radiale destra: " + Mathf.Abs (rightMinHorizontal).ToString ("n2") + "°";
-		leText.GetComponent<TextMesh> ().text = "Estensione sinistra: " + Mathf.Abs (leftMaxVertical).ToString ("n2") + "°";
-		lfText.GetComponent<TextMesh> ().text = "Flessione sinistra: " + Mathf.Abs (leftMinVertical).ToString ("n2") + "°";
-		ludText.GetComponent<TextMesh> ().text = "Dev ulnare sinistra: " + Mathf.Abs (leftMinHorizontal).ToString ("n2") + "°";
-		lrdText.GetComponent<TextMesh> ().text = "Dev radiale sinistra: " + Mathf.Abs (leftMaxHorizontal).ToString ("n2") + "°";
+		reText.GetComponent<TextMesh>().text = "Estensione destra: " + Mathf.Abs(rightMaxVertical).ToString("n2") + "°";
+		rfText.GetComponent<TextMesh>().text = "Flessione destra: " + Mathf.Abs(rightMinVertical).ToString("n2") + "°";
+		rudText.GetComponent<TextMesh>().text = "Dev ulnare destra: " + Mathf.Abs(rightMaxHorizontal).ToString("n2") + "°";
+		rrdText.GetComponent<TextMesh>().text = "Dev radiale destra: " + Mathf.Abs(rightMinHorizontal).ToString("n2") + "°";
+		leText.GetComponent<TextMesh>().text = "Estensione sinistra: " + Mathf.Abs(leftMaxVertical).ToString("n2") + "°";
+		lfText.GetComponent<TextMesh>().text = "Flessione sinistra: " + Mathf.Abs(leftMinVertical).ToString("n2") + "°";
+		ludText.GetComponent<TextMesh>().text = "Dev ulnare sinistra: " + Mathf.Abs(leftMinHorizontal).ToString("n2") + "°";
+		lrdText.GetComponent<TextMesh>().text = "Dev radiale sinistra: " + Mathf.Abs(leftMaxHorizontal).ToString("n2") + "°";
 	}
 
-	void HideStats ()
+	void HideStats()
 	{
-		reText.GetComponent<TextMesh> ().text = "";
-		rfText.GetComponent<TextMesh> ().text = "";
-		rudText.GetComponent<TextMesh> ().text = "";
-		rrdText.GetComponent<TextMesh> ().text = "";
-		leText.GetComponent<TextMesh> ().text = "";
-		lfText.GetComponent<TextMesh> ().text = "";
-		ludText.GetComponent<TextMesh> ().text = "";
-		lrdText.GetComponent<TextMesh> ().text = "";
+		reText.GetComponent<TextMesh>().text = "";
+		rfText.GetComponent<TextMesh>().text = "";
+		rudText.GetComponent<TextMesh>().text = "";
+		rrdText.GetComponent<TextMesh>().text = "";
+		leText.GetComponent<TextMesh>().text = "";
+		lfText.GetComponent<TextMesh>().text = "";
+		ludText.GetComponent<TextMesh>().text = "";
+		lrdText.GetComponent<TextMesh>().text = "";
 	}
 
-	bool CheckCoherence ()
+	bool CheckCoherence()
 	{
 		if (rightMaxVertical * rightMinVertical > 0 || rightMinHorizontal * rightMaxHorizontal > 0
 		    || leftMinVertical * leftMaxVertical > 0 || leftMinHorizontal * leftMaxHorizontal > 0)
