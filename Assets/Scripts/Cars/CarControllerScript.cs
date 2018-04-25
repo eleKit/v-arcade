be96b7bb804da7e943a7ef5f5f9fd5d8a0dcc0bd
elekit <kit93@hotmail.it>
2018-04-25 18:08:55 +0200

    car controller script

---
+++ Assets/Scripts/Cars/CarControllerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarControllerScript : MonoBehaviour
+{
+	/* 180° */
+
+	[Range (0f, 9.81f)]
+	public float gravityValue = 9.81f;
+
+	[Range (0f, 50f)]
+	public float scale = 1f;
+
+	public Text angle;
+
+
+	public GameObject handController;
+
+	private HandController hc;
+
+	private Vector3 verticalAcc;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		Physics2D.gravity = Vector3.zero;
+
+		hc = handController.GetComponent<HandController> ();
+
+		verticalAcc = new Vector3 (0f, gravityValue, 0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//controllo che il bambino giochi con una mano
+		if (hc.GetFrame ().Hands.Count == 1) {
+			float roll = hc.GetFixedFrame ().Hands.Leftmost.PalmNormal.Roll;
+			float pitch = hc.GetFixedFrame ().Hands.Leftmost.Direction.Pitch;
+			float yaw = hc.GetFixedFrame ().Hands.Leftmost.Direction.Yaw; 
+			angle.text = "Roll: " + (Mathf.Rad2Deg * roll).ToString () + "\n" +
+			"Pitch: " + (Mathf.Rad2Deg * pitch).ToString () + "\n" +
+			"Yaw: " + (Mathf.Rad2Deg * yaw).ToString ();
+
+			Vector3 horizontalAcc = new Vector3 (scale * Mathf.Sin (-roll), 0, 0);
+			Vector3 res = (horizontalAcc + verticalAcc);
+
+			Physics2D.gravity = res.normalized * gravityValue;
+			
+		} else {
+			Physics2D.gravity = Vector3.zero;
+			
+		}
+	}
+}
--- assets/Scripts/Plane/Fisio/FisioStarshipGameController.cs
+++ assets/Scripts/Plane/Fisio/FisioStarshipGameController.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class FisioStarshipGameController : MonoBehaviour {
+public class FisioStarshipGameController : MonoBehaviour
+{
 
 	string targetPrefabPath = "Prefabs/Plane/concretePipe";
-	bool testing, inGame, start, timer, pause; 
+	bool testing, inGame, start, timer, pause;
 	int time = 3;
 	int visibleHands = 0;
 	bool leftHandVisible, rightHandVisible;
@@ -16,44 +17,46 @@ public class FisioStarshipGameController : MonoBehaviour {
 	public GameObject gui, plane, info, altimeter;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		Time.timeScale = 0;
-		colors[0] = Color.yellow;
-		colors[1] = Color.blue;
-		colors[2] = Color.red;
+		colors [0] = Color.yellow;
+		colors [1] = Color.blue;
+		colors [2] = Color.red;
 		gui.SetActive (false);
 		altimeter.SetActive (false);
-		info.GetComponent<TextMesh>().text = "";
+		info.GetComponent<TextMesh> ().text = "";
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		visibleHands = GameObject.Find("MyHandController").GetComponent<MyHandController>().visibleHands;
-		leftHandVisible = GameObject.Find("MyHandController").GetComponent<MyHandController>().leftHandVisible;
-		rightHandVisible = GameObject.Find("MyHandController").GetComponent<MyHandController>().rightHandVisible;
-		if(inGame){
-			if((!PlayerSaveData.playerData.GetOneHandMode() && visibleHands < 2) ||
-			   (PlayerSaveData.playerData.GetOneHandMode() && !PlayerSaveData.playerData.GetRightHand() && !leftHandVisible) ||
-			   (PlayerSaveData.playerData.GetOneHandMode() && PlayerSaveData.playerData.GetRightHand() && !rightHandVisible) || pause)
+	void Update ()
+	{
+		visibleHands = GameObject.Find ("MyHandController").GetComponent<MyHandController> ().visibleHands;
+		leftHandVisible = GameObject.Find ("MyHandController").GetComponent<MyHandController> ().leftHandVisible;
+		rightHandVisible = GameObject.Find ("MyHandController").GetComponent<MyHandController> ().rightHandVisible;
+		if (inGame) {
+			if ((!PlayerSaveData.playerData.GetOneHandMode () && visibleHands < 2) ||
+			    (PlayerSaveData.playerData.GetOneHandMode () && !PlayerSaveData.playerData.GetRightHand () && !leftHandVisible) ||
+			    (PlayerSaveData.playerData.GetOneHandMode () && PlayerSaveData.playerData.GetRightHand () && !rightHandVisible) || pause)
 				Time.timeScale = 0f;
-			else{
+			else {
 				Time.timeScale = 1f;
-				if(!start && !timer){
-					info.GetComponent<TextMesh>().text = time.ToString();
-					InvokeRepeating("Countdown", 1f, 1f);
+				if (!start && !timer) {
+					info.GetComponent<TextMesh> ().text = time.ToString ();
+					InvokeRepeating ("Countdown", 1f, 1f);
 					timer = true;
 				}
 			}
 
 		}
 
-		if(time == 0){
-			info.GetComponent<TextMesh>().text = "";
-			CancelInvoke();
+		if (time == 0) {
+			info.GetComponent<TextMesh> ().text = "";
+			CancelInvoke ();
 			time = 3;
 			start = true;
 			timer = false;
-			plane.GetComponent<FisioPlaneScript>().SetStartAngles();
+			plane.GetComponent<FisioPlaneScript> ().SetStartAngles ();
 		}
 
 //		if(Input.GetKeyDown("escape"))
@@ -61,51 +64,59 @@ public class FisioStarshipGameController : MonoBehaviour {
 	}
 
 
-	public bool IsInGame(){
+	public bool IsInGame ()
+	{
 		return inGame;
 	}
 
-	void Begin(){
-		if(!PlayerSaveData.playerData.GetOneHandMode())
-			info.GetComponent<TextMesh>().text = "";
+	void Begin ()
+	{
+		if (!PlayerSaveData.playerData.GetOneHandMode ())
+			info.GetComponent<TextMesh> ().text = "";
 		else
-			info.GetComponent<TextMesh>().text = infoText;
+			info.GetComponent<TextMesh> ().text = infoText;
 		testing = false;
 		gui.SetActive (true);
 		altimeter.SetActive (true);
 		inGame = true;
 	}
 
-	public void BeginTest(){
+	public void BeginTest ()
+	{
 		testing = true;
 		gui.SetActive (true);
 		altimeter.SetActive (true);
 		inGame = true;
 	}
 
-	public bool IsTesting(){
+	public bool IsTesting ()
+	{
 		return testing;
 	}
 
-	void Countdown(){
+	void Countdown ()
+	{
 		time--;
-		info.GetComponent<TextMesh>().text = time.ToString();
+		info.GetComponent<TextMesh> ().text = time.ToString ();
 	}
-	void Pause(bool p){
+
+	void Pause (bool p)
+	{
 		pause = p;
 	}
 
-	public void CreatePath(string selectedPath){
-		if(selectedPath != ""){
-			List<Vector3> positions = PathSaveData.pathData.GetPathPositions(selectedPath);
-			List<Vector3> angles = PathSaveData.pathData.GetPathAngles(selectedPath);
-			for(int i = 0; i < positions.Count; i++){
-				GameObject go = (GameObject)Instantiate(Resources.Load(targetPrefabPath), positions[i], Quaternion.Euler(angles[i].x, angles[i].y, angles[i].z));
-				if(i < 10)
+	public void CreatePath (string selectedPath)
+	{
+		if (selectedPath != "") {
+			List<Vector3> positions = PathSaveData.pathData.GetPathPositions (selectedPath);
+			List<Vector3> angles = PathSaveData.pathData.GetPathAngles (selectedPath);
+			for (int i = 0; i < positions.Count; i++) {
+				GameObject go = (GameObject)Instantiate (Resources.Load (targetPrefabPath), positions [i], Quaternion.Euler (angles [i].x, angles [i].y, angles [i].z));
+				if (i < 10)
 					go.name = "Target0" + i;
 				else
 					go.name = "Target" + i;
-				go.GetComponent<Renderer>().material.color = colors[i%3];
+				go.GetComponent<Renderer> ().material.color = colors [i % 3];
 			}
 		}
 	}
