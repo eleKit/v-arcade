528111cb8598afc0413ddb90a50405ffe688174c
elekit <kit93@hotmail.it>
2018-05-06 17:23:13 +0200

    start work on the new tuning scene

---
+++ Assets/Scripts/EleTuning/TuningManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TuningManager : MonoBehaviour
+{
+
+	[Header ("Images about the hand movement")]
+	public GameObject[] m_hands_images;
+
+
+	[Header ("Intructions about the hand movement")]
+	public GameObject[] m_instructions_screen;
+
+	[Header ("Seconds the player has to wait")]
+	public Text timer;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
