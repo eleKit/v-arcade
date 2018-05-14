4a2b9f6eb837e0aaec32d56f5235ca9d0dff3e7d
elekit <kit93@hotmail.it>
2018-05-14 07:30:57 +0200

    work on path generator GUI and on car path generator

---
+++ Assets/Scripts/Path_generator/CarPathGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPathGenerator : MonoBehaviour
+{
+	public GameObject diamond;
+
+	float trajectory_length = 1f;
+
+	int n_items = 5;
+
+	float amplitude = 1.5f;
+
+	//values: -1, 0, 1
+	int[] curve_position = new int[] { 0, 1, -1 };
+
+	// Use this for initialization
+	void Start ()
+	{
+		//diamond coordinates
+		float y = 0;
+		float x = 0;
+
+		for (int h = 0; h < curve_position.Length; h++) {
+			for (int i = 0; i < n_items; i++) {
+				y = i * (trajectory_length / n_items);
+				x = amplitude *
+				Mathf.Sin (((Mathf.PI * 2) / trajectory_length) * y + ((Mathf.PI / 2) * (-curve_position [h])))
+				+ curve_position [h];
+
+				Instantiate (diamond, new Vector3 (x, y, 0), Quaternion.identity);
+			
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Path_generator/PathGeneratorGUI.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PathGeneratorGUI : MonoBehaviour
+{
+
+	/* car screen elements */
+
+
+	int curve_amplitude = 1;
+
+
+	/* shooting screen elements */
+	const int N = 3;
+	const int M = 6;
+	public bool[,] m_ducks_position = new bool[N, M];
+
+
+
+	/* initial screen*/
+
+
+	public GameObject m_initial_BG;
+
+	public GameObject m_save_screen;
+
+	public GameObject m_duck_screen;
+
+	public GameObject m_duck_instruction_screen;
+
+	public GameObject m_duck_path_screen;
+
+	public GameObject m_car_screen;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		ClearScreens ();
+		m_initial_BG.SetActive (true);
+
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+
+	public void LoadCar ()
+	{
+		ClearScreens ();
+		m_car_screen.SetActive (true);
+	}
+
+	public void LoadDuck ()
+	{
+		ClearScreens ();
+		m_duck_screen.SetActive (true);
+		ResetScreens ();
+	}
+
+	public void LoadMenu ()
+	{
+		ClearScreens ();
+		m_initial_BG.SetActive (true);
+	}
+
+	public void LoadMainMenu ()
+	{
+		SceneManager.LoadSceneAsync ("Main_Menu");
+	}
+
+	/* car screen scripts */
+
+
+	public void AddCarCurveAmplitude ()
+	{
+		curve_amplitude++;
+	}
+
+	public void CarPathSave ()
+	{
+		m_save_screen.SetActive (true);
+		
+	}
+
+	public void DuckPathSave ()
+	{
+		m_save_screen.SetActive (true);
+
+	}
+
+
+	void ClearScreens ()
+	{
+		if (m_initial_BG != null)
+			m_initial_BG.SetActive (false);
+
+		if (m_duck_screen != null)
+			m_duck_screen.SetActive (false);
+
+		if (m_car_screen != null)
+			m_car_screen.SetActive (false);
+
+		if (m_save_screen != null)
+			m_save_screen.SetActive (false);
+	}
+
+	void ResetScreens ()
+	{
+		if (m_duck_screen != null) {
+			m_duck_instruction_screen.SetActive (false);
+			m_duck_path_screen.SetActive (false);
+			StartCoroutine (LoadDucks ());
+		}
+	}
+
+	IEnumerator LoadDucks ()
+	{
+		m_duck_instruction_screen.SetActive (true);
+
+		yield return new WaitForSeconds (3f);
+
+		m_duck_instruction_screen.SetActive (false);
+
+		m_duck_path_screen.SetActive (true);
+	}
+
+
+
+}
