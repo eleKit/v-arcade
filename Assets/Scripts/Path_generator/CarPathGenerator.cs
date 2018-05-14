5094f2f67cc95b023753c7847a5965b8edd93ebd
elekit <kit93@hotmail.it>
2018-05-14 07:47:00 +0200

    car path generator script

--- Assets/Scripts/Path_generator/CarPathGenerator.cs
+++ Assets/Scripts/Path_generator/CarPathGenerator.cs
@@ -6,32 +6,36 @@ public class CarPathGenerator : MonoBehaviour
 {
 	public GameObject diamond;
 
-	float trajectory_length = 1f;
+	//this is the whole length of the game must not be changed
+	float trajectory_length = 140f;
 
-	int n_items = 5;
+	int n_items = 50;
 
-	float amplitude = 1.5f;
+	float amplitude = 10f;
 
 	//values: -1, 0, 1
-	int[] curve_position = new int[] { 0, 1, -1 };
+	int[] curve_position = new int[] { -1, 1, -1 };
 
 	// Use this for initialization
 	void Start ()
 	{
 		//diamond coordinates
-		float y = 0;
+		float y = 1f;
 		float x = 0;
 
+		int y_start = Mathf.RoundToInt (y);
+
 		for (int h = 0; h < curve_position.Length; h++) {
-			for (int i = 0; i < n_items; i++) {
+			for (int i = y_start; i < n_items; i++) {
 				y = i * (trajectory_length / n_items);
 				x = amplitude *
 				Mathf.Sin (((Mathf.PI * 2) / trajectory_length) * y + ((Mathf.PI / 2) * (-curve_position [h])))
 				+ curve_position [h];
 
-				Instantiate (diamond, new Vector3 (x, y, 0), Quaternion.identity);
+				Instantiate (diamond, new Vector3 (x, y + 1, 0), Quaternion.identity);
 			
 			}
+			y_start = Mathf.RoundToInt (y);
 		}
 		
 	}
