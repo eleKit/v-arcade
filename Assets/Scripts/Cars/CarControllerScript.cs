60a8233a0c2831c4a39b750eb5949153f6d40d60
elekit <kit93@hotmail.it>
2018-05-30 16:31:47 +0200

    now car game is playable with hand position at 180° (yaw) or 90°(pitch) or roll

--- Assets/Scripts/Cars/CarControllerScript.cs
+++ Assets/Scripts/Cars/CarControllerScript.cs
@@ -32,10 +32,17 @@ public class CarControllerScript : MonoBehaviour
 
 	private Vector3 verticalAcc;
 
+	bool ninety_deg_hand, one_hundred_and_eighty_hand, roll_hand;
+
 
 	// Use this for initialization
 	void Start ()
 	{
+		ninety_deg_hand = false;
+		one_hundred_and_eighty_hand = false;
+		roll_hand = false;
+
+
 		Physics2D.gravity = Vector3.zero;
 
 		hc = handController.GetComponent<HandController> ();
@@ -52,8 +59,22 @@ public class CarControllerScript : MonoBehaviour
 			float pitch = hc.GetFixedFrame ().Hands.Leftmost.Direction.Pitch;
 			float yaw = hc.GetFixedFrame ().Hands.Leftmost.Direction.Yaw; 
 
-			Vector3 horizontalAcc = new Vector3 (scale * Mathf.Sin (-roll), 0, 0);
-			Vector3 res = (horizontalAcc + verticalAcc);
+			Vector3 horizontalAcc = Vector3.zero;
+			Vector3 res = Vector3.zero;
+
+			if (roll_hand) {
+				horizontalAcc = new Vector3 (scale * Mathf.Sin (-roll), 0, 0);
+				res = (horizontalAcc + verticalAcc);
+			} else if (one_hundred_and_eighty_hand) {
+				horizontalAcc = new Vector3 (scale * Mathf.Sin (yaw), 0, 0);
+				res = (horizontalAcc + verticalAcc);
+			} else if (ninety_deg_hand) {
+
+				//TODO fix pich doesnt work!!
+				horizontalAcc = new Vector3 (scale * 10 * Mathf.Sin (pitch), 0, 0);
+				res = (horizontalAcc + verticalAcc);
+
+			}
 
 			Physics2D.gravity = res.normalized * gravityValue;
 			
@@ -67,4 +88,28 @@ public class CarControllerScript : MonoBehaviour
 	{
 		Physics2D.gravity = Vector3.zero;
 	}
+
+
+	public void NinetyTrue ()
+	{
+		ninety_deg_hand = true;
+		one_hundred_and_eighty_hand = false;
+		roll_hand = false;
+	}
+
+	public void OneHundredEightyTrue ()
+	{
+		ninety_deg_hand = false;
+		one_hundred_and_eighty_hand = true;
+		roll_hand = false;
+	}
+
+	public void RollTrue ()
+	{
+		ninety_deg_hand = false;
+		one_hundred_and_eighty_hand = false;
+		roll_hand = true;
+	}
+
+
 }
