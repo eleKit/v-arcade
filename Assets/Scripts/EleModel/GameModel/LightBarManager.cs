d8fc468b725a87a4763c0e6376fc6ddc600136c5
elekit <kit93@hotmail.it>
2018-06-02 16:04:20 +0200

    new coloured line at the bottom of the game screen: green if hands are visible, red otherwise

---
+++ Assets/Scripts/EleModel/GameModel/LightBarManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBarManager : MonoBehaviour
+{
+
+
+	public HandController hc;
+
+	public Image colour_line;
+
+	// Use this for initialization
+	void Start ()
+	{
+		colour_line.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+
+		if (colour_line.color.Equals (Color.green)) {
+
+			if (hc.GetFixedFrame ().Hands.Count == 0) {
+				colour_line.color = Color.red;
+			} else if (hc.GetFixedFrame ().Hands.Count == 1 && GameManager.Instance.music) {
+				colour_line.color = Color.red;
+			}
+		}
+
+		if (colour_line.color.Equals (Color.red)) {
+
+			if (hc.GetFixedFrame ().Hands.Count == 2 && GameManager.Instance.music) {
+				colour_line.color = Color.green;
+			} else if (hc.GetFixedFrame ().Hands.Count == 1
+			           && (GameManager.Instance.shooting || GameManager.Instance.car)) {
+				colour_line.color = Color.green;
+			}
+		}
+	}
+			
+}
